18e29fb9d7ad81634ef70d83263d72ea05a9a93b
Mark Collin <mark@ardescosolutions.com>
2011-11-26 10:29:03 +0000

    .gitIgnore file added Copyright updated Failing test fixed,

--- ebselen-ide-convertor/src/test/java/com/lazerycode/ebselen/codegenerators/EbselenGeneratorTest.java
+++ ebselen-ide-convertor/src/test/java/com/lazerycode/ebselen/codegenerators/EbselenGeneratorTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2010-2011 Ardesco Solutions - http://www.ardescosolutions.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.lazerycode.ebselen.codegenerators;
 
 import org.junit.Test;
@@ -8,4 +24,10 @@ import static org.junit.Assert.assertThat;
 
 public class EbselenGeneratorTest {
 
+    private EbselenGenerator ebselen = new EbselenGenerator("ebselen");
+
+    @Test
+    public void foo() {
+        assertThat(ebselen.window().takeScreenshot(), is(equalTo("ebselen.window().takeScreenshot()")));
+    }
 }
--- ebselen-tests/src/test/java/com/lazerycode/ebselen/website/google/GoogleExample.java
+++ ebselen-tests/src/test/java/com/lazerycode/ebselen/website/google/GoogleExample.java
@@ -1,17 +1,17 @@
 /*
-Copyright 2010-2012 Ardesco Solutions - http://www.ardescosolutions.com
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+ * Copyright (c) 2010-2011 Ardesco Solutions - http://www.ardescosolutions.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.lazerycode.ebselen.website.google;
 
@@ -26,6 +26,7 @@ import com.lazerycode.ebselen.SeleniumTestAnnotations.Order;
 import com.lazerycode.ebselen.google.HomePage;
 
 
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.support.PageFactory;
 
 /**
@@ -59,5 +60,6 @@ public class GoogleExample extends EbselenTestBase {
     public void searchGoogle() throws Exception {
         String searchTerm = "Test Search";
         google.searchFor(searchTerm);
+        driver.manage().window().setSize(new Dimension(1024, 768));
     }
 }

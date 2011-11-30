a35044a3c2f3baa968a12b786c4959c243be0b49
Mark Collin <mark@ardescosolutions.com>
2011-11-30 23:05:49 +0000

    Added a very basic Excel handler. JXL added to the POM file

---
+++ ebselen-core/src/main/java/com/lazerycode/ebselen/customhandlers/ExcelHandler.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2010-2011 Ardesco Solutions - http://www.ardescosolutions.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.lazerycode.ebselen.customhandlers;
+
+import jxl.Sheet;
+import jxl.Workbook;
+
+import java.io.File;
+import java.util.HashMap;
+
+public class ExcelHandler {
+
+    private HashMap<String, Sheet> importedSheets;
+    private Sheet selectedSheet = null;
+
+    public void ExcelHandler(File excelWorkbook) throws Exception {
+        Workbook workbook = Workbook.getWorkbook(excelWorkbook);
+        String[] sheetNames = workbook.getSheetNames();
+        Sheet[] sheetData = workbook.getSheets();
+        if (sheetData.length != sheetNames.length) {
+            throw new Exception("Cannot map sheets to sheet names");
+        }
+        for (int sheetNumber = 0; sheetNumber < sheetData.length; sheetNumber++) {
+            importedSheets.put(sheetNames[sheetNumber], sheetData[sheetNumber]);
+        }
+    }
+
+    public void selectSheet(String sheetName) throws Exception {
+        if (importedSheets.containsKey(sheetName)) {
+            this.selectedSheet = importedSheets.get(sheetName);
+        } else {
+            throw new Exception("Sheet with name '" + sheetName + "' doesn't exist!");
+        }
+    }
+
+    public String getCellData(int column, int row) throws Exception {
+        if(this.selectedSheet.equals(null)){
+            throw new Exception("No sheet selected.  You must select a sheet before trying to get data!");
+        }
+        return this.selectedSheet.getCell(column, row).toString();
+    }
+
+}

cf1eb686865b357b3b5055f4fe9e67f8df55d904
Mark Collin <mark@ardescosolutions.com>
2011-12-02 07:22:04 +0000

    Updated UserHandlerTest with more tests.

--- ebselen-core/src/test/java/com/lazerycode/ebselen/handlers/userhandler/UserHandlerTest.java
+++ ebselen-core/src/test/java/com/lazerycode/ebselen/handlers/userhandler/UserHandlerTest.java
@@ -62,6 +62,14 @@ public class UserHandlerTest {
     }
 
     @Test
+    public void testModifyUsernameIsCorrect() throws Exception {
+        UserHandler userObject = new UserHandler("Bill", "@lazeryattack.com", "FFOW");
+        userObject.setUserName("William");
+        assertThat(userObject.getRawUsername(), is(equalTo("William")));
+        assertThat(userObject.getUserName(), is(equalTo("William_FFOW")));
+    }
+
+    @Test
     public void testEnvironmentalHashAppliedToEmail() throws Exception {
         UserHandler userObject = new UserHandler("Bill", "@lazeryattack.com", "FFOW");
         assertThat(userObject.getEmail(), is(equalTo("Bill_FFOW@lazeryattack.com")));
@@ -77,6 +85,54 @@ public class UserHandlerTest {
     public void testDefaultEmailDomainIsSet() throws Exception {
         UserHandler userObject = new UserHandler("Bill", "FFOW");
         assertThat(userObject.getRawEmail(), is(equalTo("Bill@test.com")));
+    }
+
+    @Test
+    public void testSetDescription() throws Exception {
+        UserHandler userObject = new UserHandler("Bill", "FFOW");
+        String description = "This is my description";
+        userObject.setDescription(description);
+        assertThat(userObject.getDescription(), is(equalTo(description)));
+    }
+
+    @Test
+    public void testSetFirstName() throws Exception {
+        UserHandler userObject = new UserHandler("Bill", "FFOW");
+        String firstName = "Willian";
+        userObject.setFirstName(firstName);
+        assertThat(userObject.getFirstName(), is(equalTo(firstName)));
+    }
+
+    @Test
+    public void testSetLastName() throws Exception {
+        UserHandler userObject = new UserHandler("Bill", "FFOW");
+        String lastName = "Buthton-Smith";
+        userObject.setLastName(lastName);
+        assertThat(userObject.getLastName(), is(equalTo(lastName)));
+    }
+
+    @Test
+    public void testSetPassword() throws Exception {
+        UserHandler userObject = new UserHandler("Bill", "FFOW");
+        String password = "Password1";
+        userObject.setPassword(password);
+        assertThat(userObject.getPassword(), is(equalTo(password)));
+    }
+
+    @Test
+    public void testSetTelephoneNumber() throws Exception {
+        UserHandler userObject = new UserHandler("Bill", "FFOW");
+        String telephoneNumber = "0844 6751122";
+        userObject.setTelephoneNumber(telephoneNumber);
+        assertThat(userObject.getTelephoneNumber(), is(equalTo(telephoneNumber)));
+    }
+
+    @Test
+    public void testSetUserID() throws Exception {
+        UserHandler userObject = new UserHandler("Bill", "FFOW");
+        String userID = "125";
+        userObject.setUserID(userID);
+        assertThat(userObject.getUserID(), is(equalTo(userID)));
     }
 
 }
